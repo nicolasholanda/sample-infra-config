22e3936eb991c5dfb0309be2cbe8b01003984c09
Nicolas Holanda <nikolas.nhm14@gmail.com>
2025-08-04 09:55:33 -0300

    Fixed outputs.tf

--- tofu/outputs.tf
+++ tofu/outputs.tf
@@ -1,15 +1,22 @@
 output "prometheus_url" {
-  value = "http://prometheus-server.${var.infra_namespace}.svc.cluster.local"
+  value       = "http://prometheus-server.${var.infra_namespace}.svc.cluster.local"
   description = "Cluster-internal Prometheus URL"
 }
 
 output "grafana_url" {
-  value = "http://grafana.${var.infra_namespace}.svc.cluster.local"
+  value       = "http://grafana.${var.infra_namespace}.svc.cluster.local"
   description = "Cluster-internal Grafana URL"
 }
 
+data "kubernetes_secret" "grafana_admin" {
+  metadata {
+    name      = "grafana"
+    namespace = var.infra_namespace
+  }
+}
+
 output "grafana_admin_password" {
-  value       = helm_release.grafana.status[0].resources["Secret"]["grafana"].data["admin-password"]
-  description = "Grafana admin password (base64 encoded, decode to use)"
+  value       = base64decode(data.kubernetes_secret.grafana_admin.data["admin-password"])
+  description = "Grafana admin password"
   sensitive   = true
 }
