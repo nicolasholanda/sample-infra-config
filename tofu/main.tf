8f291cf019273b859129e023c25c54310bed1673
Nicolas Holanda <nikolas.nhm14@gmail.com>
2025-08-06 22:33:02 -0300

    Removing ns creation

--- tofu/main.tf
+++ tofu/main.tf
@@ -1,14 +1,8 @@
-resource "kubernetes_namespace" "infra" {
-  metadata {
-    name = var.infra_namespace
-  }
-}
-
 resource "helm_release" "prometheus" {
   name       = var.prometheus_release_name
   repository = var.prometheus_chart_repo
   chart      = "prometheus"
-  namespace  = kubernetes_namespace.infra.metadata[0].name
+  namespace  = var.infra_namespace
   version    = var.prometheus_chart_version
   create_namespace = false
 }
@@ -17,7 +11,7 @@ resource "helm_release" "grafana" {
   name       = var.grafana_release_name
   repository = var.grafana_chart_repo
   chart      = "grafana"
-  namespace  = kubernetes_namespace.infra.metadata[0].name
+  namespace  = var.infra_namespace
   version    = var.grafana_chart_version
   create_namespace = false
 }
